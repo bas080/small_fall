06624fc3f78d4e7c60d11bf7346cfe358f5e325b
bas080 <bassimhuis@gmail.com>
2026-04-05 17:45:40 +0300

    Add global API for small_fall
    Improve tests to find likely water falls.

--- check.lua
+++ check.lua
@@ -1,25 +1,42 @@
-local property, gen = luanti_check()
+local test, gen = luanti_check()
 
 local range = 40
 
-property("Spawns waterfall in expected places", function(t)
-    local player = gen.player_pos({
-        pos = { y = range + 2 },
-    })
+local function neighbors_are_clear(pos)
+	local dirs = {
+		{ x = 1, y = 0, z = 0 },
+		{ x = -1, y = 0, z = 0 },
+		{ x = 0, y = 1, z = 0 },
+		{ x = 0, y = -1, z = 0 },
+		{ x = 0, y = 0, z = 1 },
+		{ x = 0, y = 0, z = -1 },
+	}
 
-    t.on_emerge(function()
-        t.done(player)
-    end)
-end, function(t, player)
-    local pos = player:get_pos()
+	for _, d in ipairs(dirs) do
+		local npos = vector.add(pos, d)
+		local nodename = core.get_node(npos).name
+		if nodename == "default:water_source" then
+			return false
+		end
+	end
+	return true
+end
 
-    -- core.after(function()
-    local node_pos = core.find_node_near(pos, range, "default:water_source", true)
+test("Spawns waterfall in expected places", function(t)
+	local player, pos = gen.player_pos({
+		pos = { y = range + 2 },
+	})
 
-    -- Retry if the node was a sea level node.
-    -- Bad idea? Cannot use this in an after
-    gen.where(node_pos ~= nil)
+	local function on_emerge()
+		local node_pos = core.find_node_near(pos, range, "default:water_source", true)
 
-    t.done() -- Reached the end. It seems you have spawned a waterfall.
-    -- end)
+		if node_pos == nil or not neighbors_are_clear(node_pos) then
+			return t.retry("Did not find a likely water fall")
+		end
+
+		player:set_pos(node_pos)
+		t.done("Found a likely water fall")
+	end
+
+	t.emerge(on_emerge)
 end)
--- init.lua
+++ init.lua
@@ -1,53 +1,55 @@
 local register_decoration = luanti_utils.dofile("register_decoration.lua")
 
 local dirs = {
-    { x = 1, y = 0, z = 0 },
-    { x = -1, y = 0, z = 0 },
-    { x = 0, y = 0, z = 1 },
-    { x = 0, y = 0, z = -1 },
+	{ x = 1, y = 0, z = 0 },
+	{ x = -1, y = 0, z = 0 },
+	{ x = 0, y = 0, z = 1 },
+	{ x = 0, y = 0, z = -1 },
 }
 
 local function try_place_water(pos)
-    local check_pos = vector.new(pos)
+	local check_pos = vector.new(pos)
 
-    check_pos.y = check_pos.y + 1
+	check_pos.y = check_pos.y + 1
 
-    for i = 1, 50 do
-        local below_node = core.get_node(check_pos)
+	for i = 1, 50 do
+		local below_node = core.get_node(check_pos)
 
-        if below_node.name ~= "air" then
-            return false
-        end
+		if below_node.name ~= "air" then
+			return false
+		end
 
-        check_pos.y = check_pos.y + 1
+		check_pos.y = check_pos.y + 1
 
-        if i > 7 then
-            local dir = dirs[math.random(#dirs)]
+		if i > 7 then
+			local dir = dirs[math.random(#dirs)]
 
-            local p = vector.add(check_pos, dir)
-            local n = core.get_node(vector.add(check_pos, dir))
+			local p = vector.add(check_pos, dir)
+			local n = core.get_node(vector.add(check_pos, dir))
 
-            if core.get_item_group(n.name, "stone") > 0 then
-                core.set_node(p, { name = "default:water_source" })
-                return true
-            end
-        end
-    end
+			if core.get_item_group(n.name, "stone") > 0 then
+				core.set_node(p, { name = "default:water_source" })
+				return true
+			end
+		end
+	end
 
-    return false
+	return false
 end
 
 register_decoration({
-    name = "small_fall:waterfall",
-    place_on = { "default:water_source" },
-    deco_type = "simple",
-    fill_ratio = 0.01, -- controls how often waterfalls spawn
-    y_min = 0,
-    y_max = 1,
-    flags = "liquid_surface",
-    on_position = function(pos)
-        try_place_water(pos)
-    end,
+	name = "small_fall:waterfall",
+	place_on = { "default:water_source" },
+	deco_type = "simple",
+	fill_ratio = 0.01, -- controls how often waterfalls spawn
+	y_min = 0,
+	y_max = 1,
+	flags = "liquid_surface",
+	on_position = function(pos)
+		try_place_water(pos)
+	end,
 })
 
-return try_place_water
+_G.small_fall = {
+	try_spawn = try_place_water,
+}
